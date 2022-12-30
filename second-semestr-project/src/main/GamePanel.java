bd207c17e95b2e6e5fc99e3d2cf5b2a59bc77252
Artem Tabachenko <fexo5000@icloud.com>
2022-12-30 04:45:43 +0300

    movement added

--- second-semestr-project/src/main/GamePanel.java
+++ second-semestr-project/src/main/GamePanel.java
@@ -11,33 +11,62 @@ public class GamePanel extends JPanel implements Runnable {
     final int maxScreenRow = 13; // Разделение окна на 12 равных частей;
     final int screenWidth = titleSize * maxScreenColumn;
     final int screenHeight = titleSize * maxScreenRow;
+    KeyHandler keyHandler = new KeyHandler();
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.gray);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     Thread gameThread;
-
     public void startGameThread() {
-        gameThread = new Thread();
+        gameThread = new Thread(this);
         gameThread.start();
     }
 
+    int mouseX = 100;
+    int mouseY = 100;
+    int mouseSpeed = 10;
+    int FPS = 60;
+
     @Override
     public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+
+
         while (gameThread != null) {
 
-            update();
+            currentTime = System.nanoTime();
+//            System.out.println("Current Time is: " + currentTime );
 
-            repaint();
+            delta += (currentTime - lastTime) / drawInterval;
 
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
         }
     }
 
     public void update() {
 
+        if (keyHandler.leftPressed == true) {
+            mouseX -= mouseSpeed;
+        }
+        else if (keyHandler.rightPressed == true) {
+            mouseX += mouseSpeed;
+        }
     }
 
     public void paintComponent(Graphics graphics) {
@@ -47,7 +76,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         graphics2D.setColor(Color.white);
 
-        graphics2D.fillRect(384, 520, titleSize, titleSize);
+        graphics2D.fillRect(mouseX, mouseY, titleSize, titleSize);
 
         graphics2D.dispose();
     }
